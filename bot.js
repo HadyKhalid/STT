863cef68f1bcd2f7c592030ae97eecaa112e0f9a
HadyKhalid <45401472+HadyKhalid@users.noreply.github.com>
2018-12-16 20:29:58 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Eris = require("eris");
+var x5bzteam = new Eris("NDY1OTk5OTk2NDAwMTczMDY2.Dvgokg.h3R2sSXRp74K_wGiQhUCbRuomZw");
+x5bzteam.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
+});
+x5bz = "486763640058413068";
+nick = ">>>";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 1000);
+}
+x5bzteam.connect();
